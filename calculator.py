3d7ff03f53a84056a766cabf56c89d7ad29aab6e
stymsaw-fortmindz <satyam.saw@fortmindz.in>
2025-10-07 13:08:00 +0530

    Initial Commit

---
+++ calculator.py
@@ -0,0 +1,5 @@
+#this is a calculator program
+num1 = int(input("Enter the first Number : "))
+num2 = int(input("Enter the Second Number : "))
+sum=num1+num2
+print("The sum of Numbers is ",sum)
---
+++ hello.py
@@ -0,0 +1,4 @@
+name = input("Enter your name : ")
+name = "Ram"
+const = "Sahil"
+print("Hello", name,const)
