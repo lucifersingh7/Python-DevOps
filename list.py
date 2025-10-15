99ec0899d55c79324ce79e87274a5a7497db5d27
stymsaw-fortmindz <satyam.saw@fortmindz.in>
2025-10-15 12:50:41 +0530

    Initial Commit

---
+++ list.py
@@ -0,0 +1,16 @@
+#print a list
+
+list_clouds = ["aws",25.12,"azure","oracle",18,"bubble"]
+
+print(list_clouds)
+
+# add data at the end of list
+list_clouds.append("SalesForce")
+print(list_clouds)
+
+#finding the length of list
+length=len(list_clouds)
+print(length)
+
+list_clouds.insert(3,"007")
+print(list_clouds)
