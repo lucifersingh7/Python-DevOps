fcffbb325057a98b7b8e56585f5cecd7f959b33f
stymsaw-fortmindz <satyam.saw@fortmindz.in>
2025-10-22 13:32:01 +0530

    New question added

---
+++ ProductOfNumbers.py
@@ -0,0 +1,19 @@
+#Write a Python function to multiply all the numbers in a list.
+
+first_num = int(input("Enter number : "))
+second_num = int(input("Enter number : "))
+third_num = int(input("Enter number : "))
+fourth_num = int(input("Enter number : "))
+fifth_num = int(input("Enter number : "))
+sixth_num = int(input("Enter number : "))
+seventh_num = int(input("Enter number : "))
+
+numbers = [first_num,second_num,third_num,fourth_num,fifth_num,sixth_num,seventh_num]
+
+def show_product(numbers) :
+    result = 1
+    for i in numbers :
+        result = result * i
+    return result
+
+print("The product is", show_product(numbers))
