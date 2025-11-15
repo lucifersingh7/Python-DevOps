bcf3b4ac33beb3f175c7096028aadec030aa7282
stymsaw-fortmindz <satyam.saw@fortmindz.in>
2025-11-15 19:34:07 +0530

    Backup using Python code added

--- utils.py
+++ utils.py
@@ -1,4 +1,20 @@
 #importing a library to code
 import os
+import datetime
 #add commands depending on the OS
-print(os.system('systeminfo'))
+#print(os.system('systeminfo'))
+
+#creating functions for OS commands
+
+def check_systemInfo() :
+    print(os.system('systeminfo'))
+
+#def check_date():
+   # print(os.system('date'))
+
+
+check_systemInfo()
+#check_date()
+
+today = datetime.date.today()
+print(today)
